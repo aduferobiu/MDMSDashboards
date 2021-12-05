b12ec53ecb5dbf085ca6d2e44ab1ec7810ce8224
Adufe Robiu <aduferobiu@gmail.com>
2021-12-05 14:38:32 +0100

    (new Feature) Worked on the receptionist patient info and staff settings

---
+++ Js/deepcalendar.js
@@ -0,0 +1,6 @@
+import Calendar from 'tui-calendar'; /* ES6 */
+import "tui-calendar/dist/tui-calendar.css";
+
+// If you use the default popups, use this.
+import 'tui-date-picker/dist/tui-date-picker.css';
+import 'tui-time-picker/dist/tui-time-picker.css';
